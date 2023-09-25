cc98d6dae943ce360538ce620307ed80a12b4419
lahaina <nattolecats@gmail.com>
2023-09-26 02:07:16 +0900

    2byTricks: Add battery style changer && Refactor

--- src/jp/project2by2/CustomSettings/CustomSettings.java
+++ src/jp/project2by2/CustomSettings/CustomSettings.java
@@ -10,6 +10,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.content.Intent;
@@ -21,6 +22,7 @@ import android.os.SystemProperties;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.MenuItem;
+import android.provider.Settings;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceGroup;
@@ -33,15 +35,22 @@ import java.util.Arrays;
 
 import jp.project2by2.CustomSettings.R;
 import jp.project2by2.CustomSettings.utils.Utils;
+import jp.project2by2.CustomSettings.preference.SystemSettingListPreference;
 
 public class CustomSettings extends PreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
-    private static final String TAG = CustomSettings.class.getSimpleName();
+    private static final String TAG = "2byTricks";
 
-    public static final String KEY_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
+    private ContentResolver mContentResolver;
 
-    private SwitchPreference mPhotosSpoofingSwitch;
+    public static final String KEY_STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    public static final String KEY_STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+
+    private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+
+    private SystemSettingListPreference mStatusBarBattery;
+    private SystemSettingListPreference mStatusBarBatteryShowPercent;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -49,19 +58,31 @@ public class CustomSettings extends PreferenceFragment
 
         SharedPreferences prefs = getActivity().getSharedPreferences("main", Activity.MODE_PRIVATE);
 
-        mPhotosSpoofingSwitch = findPreference(KEY_PHOTOS_SPOOF);
-        mPhotosSpoofingSwitch.setChecked(SystemProperties.getBoolean(KEY_PHOTOS_SPOOF, false));
-        mPhotosSpoofingSwitch.setOnPreferenceChangeListener(this);
+        mContentResolver = getActivity().getContentResolver();
+
+        mStatusBarBatteryShowPercent = findPreference(KEY_STATUS_BAR_SHOW_BATTERY_PERCENT);
+        mStatusBarBattery = findPreference(KEY_STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(mStatusBarBattery.getIntValue(0));
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mPhotosSpoofingSwitch) {
-            SystemProperties.set(KEY_PHOTOS_SPOOF, String.valueOf(newValue));
-            return true;
+        int value = Integer.parseInt((String) newValue);
+        String key = preference.getKey();
+
+        switch (key) {
+            case KEY_STATUS_BAR_BATTERY_STYLE:
+                Log.d(TAG, "Set battery style: " + value);
+                enableStatusBarBatteryDependents(value);
+                break;
         }
 
-        return false;
+        return true;
+    }
+
+    private void enableStatusBarBatteryDependents(int batteryIconStyle) {
+        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
     }
 
 }
